ce276deefd42458258af93b48dac035de88e7734
Rosaria Bonito <rosaria.bonito@inaf.it>
2022-07-13 15:22:43 +0200

    Testing extreme cases

--- tests/test_models.py
+++ tests/test_models.py
@@ -38,4 +38,31 @@ def test_daily_min(test, expected):
     """Test min function works for zeroes, positive integers, mix of positive/negative integers."""
     from inflammation.models import daily_min
     npt.assert_array_equal(daily_min(np.array(test)), np.array(expected))
+
+@pytest.mark.parametrize(
+    "test, expected",
+    [
+        ([[0, 0, 0], [0, 0, 0], [0, 0, 0]], [[0, 0, 0], [0, 0, 0], [0, 0, 0]]),
+        ([[1, 1, 1], [1, 1, 1], [1, 1, 1]], [[1, 1, 1], [1, 1, 1], [1, 1, 1]]),
+        ([[1e100, 2e100, 3e100], [4e100, 5e100, 6e100], [7e100, 8e100, 9e100]], [[0.33, 0.67, 1], [0.67, 0.83, 1], [0.78, 0.89, 1]]),
+        ([[float('nan'), float('nan'), float('nan')], [float('nan'), float('nan'), float('nan')], [float('nan'), float('nan'), float('nan')]], [[float('nan'), float('nan'), float('nan')], [float('nan'), float('nan'), float('nan')], [float('nan'), float('nan'), float('nan')]]),
+        ([[-1, -2, -3], [-4, -5, -6], [-7, -8, -9]], [[-1, -2, -3], [-4,-5, -6], [-7, -8, -9]]),
+        ([[1, 2, 3], [4, 5, 6], [7, 8, 9]], [[0.33, 0.67, 1], [0.67, 0.83, 1], [0.78, 0.89, 1]]),
+    ])
+
+def patient_normalise(data):
+    """
+    Normalise patient data from a 2D inflammation data array.
+
+    NaN values are ignored, and normalised to 0.
+
+    Negative values are rounded to 0.
+    """
+    max_data = np.nanmax(data, axis=1)
+    with np.errstate(invalid='ignore', divide='ignore'):
+        normalised = data / max_data[:, np.newaxis]
+    normalised[np.isnan(normalised)] = 0
+    normalised[normalised < 0] = 0
+    return normalised
+
 ...
