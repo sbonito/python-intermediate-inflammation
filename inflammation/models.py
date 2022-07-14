ae3e0dbdebafe743fcbb2fb1ee7da53aaf0126e0
Rosaria Bonito <rosaria.bonito@inaf.it>
2022-07-14 16:27:24 +0200

    Docstring improvements.

--- inflammation/models.py
+++ inflammation/models.py
@@ -19,16 +19,293 @@ def load_csv(filename):
 
 
 def daily_mean(data):
-    """Calculate the daily mean of a 2D inflammation data array."""
+    """Calculate the daily mean of a 2D inflammation data array.
+    Mean value for each day inspected.
+    """
     return np.mean(data, axis=0)
 
 
 def daily_max(data):
-    """Calculate the daily max of a 2D inflammation data array."""
+    """Calculate the daily max of a 2D inflammation data array.
+    Maximum value for each day inspected.
+    """
     return np.max(data, axis=0)
 
 
 def daily_min(data):
-    """Calculate the daily min of a 2D inflammation data array."""
+    """Calculate the daily min of a 2D inflammation data array.
+    Minimum value for each day inspected
+    """
     return np.min(data, axis=0)
+
+def patient_normalise(data):
+    if np.any(data < 0):
+        raise ValueError('Inflammation values should not be negative')
+    """Normalise patient data from a 2D inflammation data array."""
+    max_data = np.max(data, axis=1)
+    return data / max_data[:, np.newaxis]
+
+class Patient:
+    """A patient in an inflammation study."""
+    def __init__(self, name):
+        self.name = name
+        self.observations = []
+
+    def add_observation(self, value, day=None):
+        if day is None:
+            try:
+                day = self.observations[-1]['day'] + 1
+
+            except IndexError:
+                day = 0
+
+        new_observation = {
+            'day': day,
+            'value': value,
+        }
+
+        self.observations.append(new_observation)
+        return new_observation
+
+alice = Patient('Alice')
+print(alice)
+
+observation = alice.add_observation(3)
+print(observation)
+print(alice.observations)
+
+class Patient:
+    """A patient in an inflammation study."""
+    def __init__(self, name):
+        self.name = name
+        self.observations = []
+
+    def add_observation(self, value, day=None):
+        if day is None:
+            try:
+                day = self.observations[-1]['day'] + 1
+
+            except IndexError:
+                day = 0
+
+
+        new_observation = {
+            'day': day,
+            'value': value,
+        }
+
+        self.observations.append(new_observation)
+        return new_observation
+
+    def __str__(self):
+        return self.name
+
+
+alice = Patient('Alice')
+print(alice)
+
+class Song:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+
+    def __str__(self):
+        return self.title + ' a song by ' + self.author
+
+song = Song('Wind & Trees', 'Sara Bonito - EX REI')
+
+print(song)
+
+class Observation:
+    def __init__(self, day, value):
+        self.day = day
+        self.value = value
+
+    def __str__(self):
+        return str(self.value)
+
+class Patient:
+    """A patient in an inflammation study."""
+    def __init__(self, name):
+        self.name = name
+        self.observations = []
+
+    def add_observation(self, value, day=None):
+        if day is None:
+            try:
+                day = self.observations[-1].day + 1
+
+            except IndexError:
+                day = 0
+
+        new_observation = Observation(day, value)
+
+        self.observations.append(new_observation)
+        return new_observation
+
+    def __str__(self):
+        return self.name
+
+
+alice = Patient('Alice')
+obs = alice.add_observation(3)
+
+print(obs)
+
+class Observation:
+    def __init__(self, day, value):
+        self.day = day
+        self.value = value
+
+    def __str__(self):
+        return str(self.value)
+
+class Person:
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return self.name
+
+class Doctor:
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return self.name
+
+class Patient(Person):
+    """A patient in an inflammation study."""
+    def __init__(self, name):
+        super().__init__(name)
+        self.observations = []
+
+    def add_observation(self, value, day=None):
+        if day is None:
+            try:
+                day = self.observations[-1].day + 1
+
+            except IndexError:
+                day = 0
+
+        new_observation = Observation(day, value)
+
+        self.observations.append(new_observation)
+        return new_observation
+
+alice = Patient('Alice')
+print(alice)
+
+obs = alice.add_observation(3)
+print(obs)
+
+drake = Doctor('Drake')
+print(drake)
+
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+
+    def __str__(self):
+        return self.title + ' by ' + self.author
+
+
+class Library:
+    def __init__(self):
+        self.books = []
+
+    def add_book(self, title, author):
+        self.books.append(Book(title, author))
+
+    def __len__(self):
+        return len(self.books)
+
+    def __getitem__(self, key):
+        return self.books[key]
+
+    def by_author(self, author):
+        matches = []
+        for book in self.books:
+            if book.author == author:
+                matches.append(book)
+
+        if not matches:
+            raise KeyError('Author does not exist')
+
+        return matches
+
+library = Library()
+
+library.add_book('My First Book', 'Alice')
+library.add_book('My Second Book', 'Alice')
+library.add_book('A Different Book', 'Bob')
+
+print(len(library))
+
+book = library[2]
+print(book)
+
+books = library.by_author('Alice')
+for book in books:
+    print(book)
+
+
+class Observation:
+    def __init__(self, day, value):
+        self.day = day
+        self.value = value
+
+    def __str__(self):
+        return self.value
+
+class Person:
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return self.name
+
+class Patient(Person):
+    """A patient in an inflammation study."""
+    def __init__(self, name, observations=None):
+        super().__init__(name)
+
+        self.observations = []
+        if observations is not None:
+            self.observations = observations
+
+    def add_observation(self, value, day=None):
+        if day is None:
+            try:
+                day = self.observations[-1].day + 1
+
+            except IndexError:
+                day = 0
+
+        new_observation = Observation(value, day)
+
+        self.observations.append(new_observation)
+        return new_observation
+
+def attach_names(data, names):
+    """Create datastructure containing patient records."""
+    assert len(data) == len(names)
+    output = []
+
+    for data_row, name in zip(data, names):
+        output.append({'name': name,
+                       'data': data_row})
+
+    return output
+
+data = np.array([[1., 2., 3.],
+                 [4., 5., 6.]])
+
+output = attach_names(data, ['Alice', 'Bob'])
+print(output)
+
+
+
+
 
